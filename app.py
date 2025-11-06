000a383f8746e9030f32863a9f6790826a73fc2b
logavishwa <Vishwaff07@gmail.com>
2025-11-06 15:47:13 +0530

    Use Gunicorn and correct port for Render

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from chat import get_response
 import os
 import nltk
 
-# Download nltk data
 nltk.download('punkt')
 
 app = Flask(__name__)
 
-# ---- Routes ----
 @app.route('/')
 def home():
     return render_template('base.html')
@@ -17,10 +15,8 @@ def home():
 def predict():
     data = request.get_json().get("message")
     response = get_response(data)
-    message = {"answer": response}
-    return jsonify(message)
+    return jsonify({"answer": response})
 
-# ---- Main Entry ----
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
